65b8d2de7bd2ae6ecba04693edab36cd84bfd889
Umer farooq <45381610+umerfarooq1997@users.noreply.github.com>
2024-07-29 11:34:26 +0500

    config changes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
 // eslint-disable-next-line node/prefer-global/process
 const isProd = Boolean(process.env.REACT_APP_ENV === 'prod');
-// 1c0bbce296db4f528a0b7dcf3d8feb6b
-// 5d3b122e45754fb49a3f20a5653b3176
+
+// ** if the key is not working pick another key.
+// 1) 1c0bbce296db4f528a0b7dcf3d8feb6b // Current Key
+// 2) 5d3b122e45754fb49a3f20a5653b3176
+
 const config = {
   API_KEY: isProd ? 'production key here!' : '1c0bbce296db4f528a0b7dcf3d8feb6b',
 };
